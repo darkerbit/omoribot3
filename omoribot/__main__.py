19d5a4d428568580b88483937d8b151193aedd0d
darkerbit <mail.darkerbit@gmail.com>
2022-10-14 20:18:51 +0300

    Yay security

--- omoribot/__main__.py
+++ omoribot/__main__.py
@@ -23,13 +23,32 @@ bot = commands.Bot(command_prefix='?', intents=intents)
 
 
 @bot.command()
-async def portrait(ctx: commands.Context, portrait: str):
-    tree = Box(Portrait(f"portraits/{portrait}.png"))
+async def portrait(ctx: commands.Context, portrait_name: str):
+    safe = os.path.abspath("portraits/")
+    portr = os.path.abspath(f"portraits/{portrait_name}.png")
+
+    await ctx.send(portr)
+    await ctx.send(os.path.commonpath([safe, portr]))
+
+    if os.path.commonpath([safe, portr]) != safe:
+        await ctx.reply("Can't load portraits from outside the portraits folder!")
+        return
+
+    if not os.path.exists(portr):
+        await ctx.reply("No such portrait!")
+        return
+
+    tree = Box(Portrait(portr))
 
     path = f"out/{ctx.author.id}.png"
     render(tree, path)
 
     await ctx.reply(file=discord.File(path))
+
+
+@bot.command()
+async def where_the_fuck_am_i(ctx: commands.Context):
+    await ctx.reply(f"I am running from {os.getcwd()}")
 
 
 if __name__ == '__main__':
